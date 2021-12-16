f97a8e32f7b76d7e8857cdc6d2e5ac6e02fbbc8d
Suleiman <suleimanzhukov@gmail.com>
2021-12-16 11:33:52 +0200

    starting with ui

--- app/src/main/java/com/suleimanzhukov/realestatemanagerapp/framework/MainActivity.kt
+++ app/src/main/java/com/suleimanzhukov/realestatemanagerapp/framework/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.suleimanzhukov.realestatemanagerapp
+package com.suleimanzhukov.realestatemanagerapp.framework
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.suleimanzhukov.realestatemanagerapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
